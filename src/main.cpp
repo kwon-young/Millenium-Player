426525c52697b666cbfac1b8b8a36c3e5e81e34a
Kwon-Young Choi <kwon-young.choi@hotmail.fr>
2015-04-18 14:51:50 +0200

    add license mit to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,27 @@
+/*
+ *This program is a simple music player
+ *
+ *Copyright © 2015 Kwon-Young Choi
+ *
+ *Permission is hereby granted, free of charge, to any person obtaining
+ *a copy of this software and associated documentation files (the "Software"),
+ *to deal in the Software without restriction, including without limitation
+ *the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *and/or sell copies of the Software, and to permit persons to whom the
+ *Software is furnished to do so, subject to the following conditions:
+ *
+ *The above copyright notice and this permission notice shall be included
+ *in all copies or substantial portions of the Software.
+ *
+ *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ *DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ *TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ *OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <libtools.h>
@@ -15,7 +39,7 @@ struct OutputData {
   Signal left_out;
   int playState;
   int forwardState;
-  bool push;
+  volatile bool push;
 };
 
 // Interleaved buffers
@@ -36,7 +60,8 @@ int output( void *outputBuffer, void * /*inputBuffer*/, unsigned int nBufferFram
          nBufferFrames * sizeof( MY_TYPE));
 
   oData->push=true;
-  return oData->myDecoder.ended();
+  std::cout << "sample played\n" << std::endl;
+  return 0;
 }
 
 int main(int argc, char* argv[])
@@ -47,10 +72,15 @@ int main(int argc, char* argv[])
   Signal::globalConfigurationFromPow2(48000, 512);
 
   string_t music_file;
+  std::vector<string_t> music_files;
   if (argc >1) {
-    //retrieve the name of the file to be played
-    music_file = argv[1];
+    for (int i=1; i<argc; i++) {
+      //retrieve the name of the file to be played
+      music_file = argv[i];
+      music_files.push_back(music_file);
+    }
   }
+  int current_music_file = -1;
 
   RtAudio dac;
   if ( dac.getDeviceCount() < 1 ) {
@@ -78,7 +108,7 @@ int main(int argc, char* argv[])
   }
 
   OutputData data;
-  data.myDecoder.open(music_file);
+
 
   oParams.deviceId = dac.getDefaultOutputDevice();
 
@@ -86,10 +116,12 @@ int main(int argc, char* argv[])
             << " (buffer frames = " << bufferFrames
             << ")." << std::endl;
 
-  //NEWindow window(sf::VideoMode(800, 400), "Millenium Player", sf::Font());
-  sf::RenderWindow window(sf::VideoMode(800, 400),
-                    "Millenium Player");
+  NEWindow::registerClass();
+  NEWindow window(sf::VideoMode(800, 400), "Millenium Player", sf::Font());
+  //sf::RenderWindow window(sf::VideoMode(800, 400),
+  //                  "Millenium Player");
 
+  //declare player interface
   Interface player(sf::Vector2u(400, 400),
                    sf::Vector2f(400, 400));
   sf::IntRect idle_area(0, 0, 400, 400);
@@ -116,11 +148,22 @@ int main(int argc, char* argv[])
   forward.setPosition(500, 100);
   forward.setWin98Looking(false);
   player.addMouseCatcher(&forward);
-  bool mousePresse=false, mem=false;
-  sf::Vector2i mousePos;
   play.linkTo(&(data.playState));
-  forward.linkTo(&(data.forwardState));
+  play.forceOff();
+  forward.linkTo(&(data.forwardState), ButtonMode::on);
+  window.registerInterface(player);
+  window.arrange();
 
+  GLSLRender myShader;
+  Sampler1D mySampler;
+  mySampler.create(Signal::size, NULL);
+  myShader.loadFromMemory(GLSLRender::getVertexShaderCode(), lightsaber_signal_fs_src);
+  myShader.setParameter("signal", mySampler);
+  myShader.setParameter("thickness", 0.1f);
+  myShader.setParameter("reversed", false);
+  myShader.setParameter("color", sf::Color(0, 140, 0, 140));
+  Signal copy;
+  //start rtaudio driver
   RtAudio::StreamOptions options;
   options.flags = RTAUDIO_NONINTERLEAVED;
   try {
@@ -139,52 +182,68 @@ int main(int argc, char* argv[])
               << '\n' << std::endl;
     goto cleanup;
   }
+
   //while the end of the file is not reached
   while(window.isOpen()) {
-    mousePresse=false;
+    MSG message;
+    while (PeekMessage(&message, NULL, 0, 0, PM_REMOVE)) {
+      TranslateMessage(&message);
+      DispatchMessage(&message);
+    }
+
     sf::Event event;
     while(window.pollEvent(event)) {
       switch(event.type) {
         case sf::Event::Closed:
+          window.close();
+          data.right_out.reset();
+          data.left_out.reset();
           try {
             dac.stopStream();
           } catch (RtAudioError& e) {
             std::cout << '\n' << e.getMessage()
                       << '\n' <<std::endl;
-            goto cleanup;
-          }
-          window.close();
-          break;
-        case sf::Event::MouseButtonPressed:
-          mousePos = sf::Mouse::getPosition(window);
-          if(forward.onMousePress(mousePos.x, mousePos.y)) {
-            forward.forceOn();
           }
           break;
-
-        case sf::Event::MouseButtonReleased:
-          mousePos = sf::Mouse::getPosition(window);
-          play.onMouseRelease(mousePos.x, mousePos.y);
-          forward.forceOff();
+        default:
+          window.useEvent(event);
           break;
       }
     }
 
+    if (data.forwardState || data.myDecoder.ended()) {
+      forward.forceOff();
+      current_music_file++;
+      if (current_music_file >= music_files.size()) {
+        current_music_file=0;
+      }
+      if (music_files.size()) {
+        data.myDecoder.open(music_files[current_music_file]);
+      }
+    }
     if (data.push) {
       //reset of the stereo sound
       data.right_out.reset();
       data.left_out.reset();
       //read the next sample to be played
-      if (!data.playState) {
-      data.myDecoder.fetch(data.left_out,
+      if (data.playState) {
+        data.myDecoder.fetch(data.left_out,
                              data.right_out);
-      }
+        copy = data.left_out;
+        copy.glslize();
+        mySampler.update(copy.samples, Signal::size, 0);
+      std::cout << "samples fetched\n" << std::endl;
       data.push=0;
+      }
     }
 
-    if (dac.isStreamRunning() == false) window.close();
+    if (dac.isStreamRunning() == true) {
+      std::cout << "stream is running\n" << std::endl;
+    }
     window.clear();
-    window.draw(player);
+    window.setView(sf::View(sf::FloatRect(-0.5, -0.5, 1, 1)));
+    window.draw(myShader);
+    window.drawContent();
     window.display();
     sf::sleep(sf::milliseconds(1));
   }
